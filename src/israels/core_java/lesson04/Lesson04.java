b7929cdccfc94bb39de79ed33f3633e1b2bf2a24
Israel Sanchez <Israel.pro1994@gmail.com>
2021-01-25 13:49:07 -0600

    learning about LogicOperators - AND/OR

--- src/israels/core_java/lesson04/Lesson04.java
+++ src/israels/core_java/lesson04/Lesson04.java
@@ -4,8 +4,14 @@ public class Lesson04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 		Equality testEqu = new Equality();
 		testEqu.inClassWork();
+		
+		LogicOperators logic = new LogicOperators();
+		logic.inClassWork();
+		
 	}
 
 }
